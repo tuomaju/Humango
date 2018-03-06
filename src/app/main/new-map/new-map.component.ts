a6550e69000cb8c30ce08a13e2df5efd6bbd3c9b
Sanna Luostarinen <sannaluo@ety-a2123b-18.edu.wks.metropolia.fi>
2018-03-06 15:32:45 +0200

    uusi map ja kaikki edellinen

---
+++ src/app/main/new-map/new-map.component.ts
@@ -0,0 +1,67 @@
+import {Component, OnInit} from '@angular/core';
+import {ViewChild} from '@angular/core';
+import {} from '@types/googlemaps';
+
+@Component({
+    selector: 'app-new-map',
+    templateUrl: './new-map.component.html',
+    styleUrls: ['./new-map.component.scss']
+})
+export class NewMapComponent implements OnInit {
+
+
+    @ViewChild('gmap') gmapElement: any;
+    map: google.maps.Map;
+
+    lat: any;
+    lon: any;
+    x = document.querySelector('#gmap');
+
+    ngOnInit() {
+
+        this.lat = localStorage.getItem('latitude');
+        this.lon = localStorage.getItem('longitude');
+        // console.log(Math.floor((Math.random() * 10) + 1));
+        if (!this.lat) {
+            this.lat = 60.186332588;
+            this.lon = 24.93749625;
+        }
+
+        this.getLocation();
+
+
+        setTimeout(() => {
+            const mapProp = {
+                disableDefaultUI: true,
+                center: new google.maps.LatLng(this.lat, this.lon),
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+
+        }, 250);
+    }
+
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.showPosition, this.errorCallback);
+            console.log('geolocation');
+        } else {
+            this.x.innerHTML = 'Geolocation is not supported by this browser.';
+            console.log('no geolocation');
+        }
+    }
+
+    showPosition(position) {
+        // console.log(position);
+        // console.log(position.coords.latitude);
+        localStorage.setItem('latitude', position.coords.latitude);
+        localStorage.setItem('longitude', position.coords.longitude);
+    }
+
+    errorCallback() {
+        console.log('error');
+    }
+
+
+}
