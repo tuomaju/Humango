513ae4024ec351fee7660ab90f91c7eefd53866a
Raouf Saadi <Rafster@Raouf-MacBook-Pro.local>
2018-03-09 10:29:32 +0200

    kommentit

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MainComponent } from './main/main.component';
+import { SettingsComponent } from './top-bar/top-content/settings/settings.component';
+import { HistoryComponent } from './top-bar/top-content/history/history.component';
+import { TopContentComponent } from './top-bar/top-content/top-content.component';
+import { MangotreeComponent } from './top-bar/mangotree/mangotree.component';
+import { MapComponent } from './map/map.component';
+import { LogoBoxComponent } from './logo-box/logo-box.component';
+import { ModalComponent } from './modal/modal.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { NewChallengeComponent } from './modal/new-challenge/new-challenge.component';
+import { MotivationComponent } from './motivation/motivation.component';
+import { InfoComponent } from './info/info.component';
+import { CommentComponent } from './comment/comment.component';
+import { AcceptedChallengeComponent } from './accepted-challenge/accepted-challenge.component';
+import { NewMapComponent } from './main/new-map/new-map.component';
+import {FormsModule} from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    MainComponent,
+    SettingsComponent,
+    HistoryComponent,
+    TopContentComponent,
+    MangotreeComponent,
+    MapComponent,
+    LogoBoxComponent,
+    ModalComponent,
+    NewChallengeComponent,
+    MotivationComponent,
+    InfoComponent,
+    CommentComponent,
+    AcceptedChallengeComponent,
+    NewMapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+      HttpClientModule,
+      FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+    selector: 'app-top-bar',
+    templateUrl: './top-bar.component.html',
+    styleUrls: ['./top-bar.component.scss']
+})
+export class TopBarComponent implements OnInit {
+
+
+    constructor(private router: Router,
+                private route: ActivatedRoute) {
+    }
+
+
+    ngOnInit() {
+    }
+
+    closeTop() {
+        (<HTMLElement>document.querySelector('app-top-bar')).style.top = 'calc(-100%)';
+        (<HTMLElement>document.querySelector('app-main')).style.filter = 'blur(0px)';
+        setTimeout(() => {
+            this.router.navigate(['../']);
+        }, 500);
+
+    }
+
+
+}
